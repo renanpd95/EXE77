843ab4b977105974152fa58dfcab67e8bb3eecef
Renan <88853399+renanpd95@users.noreply.github.com>
2022-05-10 15:35:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+n1 = int(input("Digite o primeiro numero:"))
+n2 = int(input("Digite o segundo numero:"))
+soma = 0
+
+for x in range(n1,n2):
+  
+ 
+  #print (x)
+  soma = soma + x
+else:
+  result = soma + n2
+  print("Resultado:",result)
